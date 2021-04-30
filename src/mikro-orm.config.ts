13d28eee9818a4687dd28d27f89bd103451963ba
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-30 19:27:26 +0100

    setup graphQL and MikroOrm for crud on postgres db

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import path from "path";
+
+const postgresql = "postgresql" as const;
+const lireddit = "lireddit" as const;
+
+export default {
+  entities: [Post],
+  dbName: lireddit,
+  type: postgresql,
+  debug: !__prod__,
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    disableForeignKeys: false,
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+};
