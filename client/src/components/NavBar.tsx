8946d9d33dec86e91290216dae91877fcdaeb7d2
scrawnycoleman <thomashutchinson450@gmail.com>
2021-05-05 22:53:10 +0100

    NavBar with Me query

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, Link } from "@chakra-ui/layout";
+import React from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+import { Button } from "@chakra-ui/button";
+
+interface NavBarProps {}
+
+const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>Welcome, {data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Box bg="purple" p={4}>
+      <Box d="flex" justifyContent="flex-end">
+        {body}
+      </Box>
+    </Box>
+  );
+};
+
+export default NavBar;
